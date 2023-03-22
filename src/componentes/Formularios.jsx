50193fe4f1d946f22b01ff76cd2dbde55f225b73
PabloMunizMontoya <119509495+PabloMunizMontoya@users.noreply.github.com>
2023-03-22 14:41:05 -0500

    usamos un on change que toma los valores del select

--- src/componentes/Formularios.jsx
+++ src/componentes/Formularios.jsx
@@ -23,15 +23,28 @@ const Boton = styled.input`
 
 const Formulario = () => {
 
+    //6. creamos una variable que sera un array de objetos con los nombres de las monedas.
+    const MONEDAS = [
+        {codigo: 'USD', nombre: 'Dolar de Estados Unidos'},
+        {codigo: 'MXN', nombre: 'Peso Mexicano'},
+        {codigo: 'EUR', nombre: 'Euro'},
+        {codigo: 'GBP', nombre: 'Libra Esterlina'}
+    ]
+
     //5.3 Utilizar useMoneda, que es el custom hook, lo hacemos con array destructuring, lo que importa aca es el orden en que se retorna en el custom hook return [ state, Seleccionar, actualizarState ], entonces para usarlo puedo re nombrar y toma el valor según la posición 0,1,2.
-    const [ moneda, SelectMonedas, actualizarState ] = useMoneda()
+    //5.4 ahora le pasamos un valor inicial a useMoneda, este en realidad seria el primer valor, pero le podemos pasar mas valores, ahora le vamos a pasar la moneda.
+    //5.7 pasamos la moneda que en este caso sera un string vació ya que ese valor sea la moneda que elige el usuario.
+    //6.1 le pasamos a nuestro custom hook el objeto MONEDAS
+    const [ moneda, SelectMonedas, actualizarState ] = useMoneda('Elige tu Moneda', '', MONEDAS)
+
+    
 
     return ( 
         <form>
 
             {/* 5.4 usamos lo que se muestra en pantalla del custom hook, que es selectMonedas, como un componente */}
             <SelectMonedas/>
-            
+
             {/* 4.1 importamos el componente creado con styled components */}
             <Boton
                 type= 'submit'
--- src/hooks/useMoneda.js
+++ src/hooks/useMoneda.js
@@ -2,17 +2,29 @@ import React, {Fragment, useState} from 'react'
 
 
 //5. creamos la function que define nuestro hook
-const useMoneda = () => {
+//5.5 extraemos el valor puesto desde Formulario 'Elige tu Moneda', entonces decimos que cuando se utiliza useMoneda toma un valor que se llama label
+//5.8 le pasamos el segundo valor que seria el state inicial, en este caso es un string vació qeu viene desde formulario.
+//6.2 le pasamos al custom hook las MONEDAS, se lo pasamos como opciones
+const useMoneda = (label, stateInicial, opciones) => {
 
     //5.2 estate de nuestro custom hook
-    const [state, actualizarState] = useState('')
+    const [state, actualizarState] = useState(stateInicial)
 
     //5.1 lo que esta aca es lo que se va a mostrar en pantalla, esto mostrara labels y selects. 
     const Seleccionar = () => (
         <Fragment>
-            <label>Moneda</label>
-            <select>
-                <option value='MXN'>Peso Mexicano</option>
+            {/* 5.6 renderizamos el label que en este caso seria elige tu moneda */} 
+            <label>{label}</label>
+            <select
+                /* 7. ponemos un onchange en el select para actualizar los datos del state, el onChange escucha los cambios en el select y el estado toma el valor del select, como state es moneda en el formulario el valor moneda se carga con el value del select */
+                onChange={e => actualizarState(e.target.value)}
+                value={state}
+            >
+                <option value=''>-- seleccione --</option>
+                {/* 6.3 pasamos opciones que es un arreglo que contiene las monedas y lo mapeamos */}
+                {opciones.map(opcion => (
+                    <option key={opcion.codigo} value= {opcion.codigo}>{opcion.nombre}</option>
+                ))}
             </select>
         </Fragment>
     )
