fc8cc34f1252bd541f5c0ea7d8311aab7783f925
PabloMunizMontoya <119509495+PabloMunizMontoya@users.noreply.github.com>
2023-03-22 23:11:13 -0500

    le puse estilo al html del customhook con styled components

--- src/hooks/useMoneda.js
+++ src/hooks/useMoneda.js
@@ -1,5 +1,26 @@
 import React, {Fragment, useState} from 'react'
+import styled from '@emotion/styled'
 
+//8. creamos el styled component para label
+const Label = styled.label`
+    font-family: 'Bebas Neue', cursive;
+    color: #fff;
+    text-transform: uppercase;
+    font-weight: bold;
+    font-size:2.4rem;
+    margin-top: 2rem;
+    display: block;
+`
+//9. creamos el styledComponents para el select
+const Select = styled.select`
+    width: 100%;
+    display: block;
+    padding: 1rem;
+    -webkit-appearance: none;
+    border-radius: 10px;
+    border: none;
+    font-size: 1.2rem;
+`
 
 //5. creamos la function que define nuestro hook
 //5.5 extraemos el valor puesto desde Formulario 'Elige tu Moneda', entonces decimos que cuando se utiliza useMoneda toma un valor que se llama label
@@ -14,8 +35,11 @@ const useMoneda = (label, stateInicial, opciones) => {
     const Seleccionar = () => (
         <Fragment>
             {/* 5.6 renderizamos el label que en este caso seria elige tu moneda */} 
-            <label>{label}</label>
-            <select
+            {/* 8.1 cambiamos el label por el styledComponent */}
+            <Label>{label}</Label>
+
+            {/* 9.1 borramos el select y ponemos el componente creado con component styled */}
+            <Select
                 /* 7. ponemos un onchange en el select para actualizar los datos del state, el onChange escucha los cambios en el select y el estado toma el valor del select, como state es moneda en el formulario el valor moneda se carga con el value del select */
                 onChange={e => actualizarState(e.target.value)}
                 value={state}
@@ -25,7 +49,7 @@ const useMoneda = (label, stateInicial, opciones) => {
                 {opciones.map(opcion => (
                     <option key={opcion.codigo} value= {opcion.codigo}>{opcion.nombre}</option>
                 ))}
-            </select>
+            </Select>
         </Fragment>
     )
 
