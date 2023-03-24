e78c744b23864fc98079a852ee3ab6c3f72f87aa
PabloMunizMontoya <119509495+PabloMunizMontoya@users.noreply.github.com>
2023-03-24 18:02:16 -0500

    termine la app

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import axios from 'axios'
 import imagen from './cryptomonedas.png'
 import Formulario from './componentes/Formularios'
 import Cotizacion from './componentes/Cotizacion'
+import Spinner from './componentes/Spinner'
 
 //1. creamos el componente styled
 const Contenedor = styled.div `
@@ -50,6 +51,9 @@ function App() {
   //18 creamos un state para guardar la relación entre el precio moneda y criptomoneda.
   const [resultado, guardarResultado] = useState ({})
 
+  //20. creamos state para cargar el spinner
+  const [cargando, guardarCargando] = useState(false)
+
   //17. creo un useEffect que va a dispararse cuando el valor moneda y criptomoneda cambien, estos cambian con su useState desde formulario
   useEffect( ( ) =>  {
     
@@ -65,14 +69,29 @@ function App() {
     //17.4 definimos el resultado en una variable usando axios y obteniendo el resultado de la url con un método get.
     const resultado = await axios.get(url)
 
-    //17.5 18.1 guardamos el resultado en el estate creado para ello, vemos que el resultado es el recorrido del objeto que nos lleva hasta lo que deseamos, este camino es dinámico, cuando cambian los valores en el form el objeto cambia, por eso en el camino ponemos de forma dinámica los valores criptomoneda y moneda.
-    guardarResultado(resultado.data.DISPLAY[criptomoneda][moneda])
+    //20.1 mostrar el spinner, en esta instancia el estado pasa a true y luego con un set time out renderizamos el spinner.
+    guardarCargando(true)
+
+    //20.2 ocultamos el spinner con un set time out, entonces después de pasar 3 seg el spinner cambia a false y desaparece.
+    setTimeout(() =>{
+
+      //20.3 volvemos el estado de cargando a false
+      guardarCargando(false)
+
+      //17.5 18.1 guardamos el resultado en el estate creado para ello, vemos que el resultado es el recorrido del objeto que nos lleva hasta lo que deseamos, este camino es dinámico, cuando cambian los valores en el form el objeto cambia, por eso en el camino ponemos de forma dinámica los valores criptomoneda y moneda.
+      guardarResultado(resultado.data.DISPLAY[criptomoneda][moneda])
+
+    }, 3000)
+    
     }
     
-    //17.5 mandamos a llamar a la function
+    //17.6 mandamos a llamar a la function
     cotizarCriptomoneda()
     
   }, [moneda, criptomoneda])
+
+  //21. creamos una variable que contiene componentes en donde si algo es true o false el valor de este componente cambia de un componente a otro. esto se llama componente condicional
+  const componente = (cargando) ? <Spinner /> : <Cotizacion resultado = {resultado}/>
 
   return (
     /* 1.2 insertamos el componente creado con styledComponents */
@@ -97,9 +116,9 @@ function App() {
           guardarMoneda={guardarMoneda}
           guardarCriptomoneda={guardarCriptomoneda}
         />
-        <Cotizacion
-          resultado = {resultado}
-        />
+        
+        {/* 21.2 mostramos en pantalla el componente condicional */}
+        {componente}
       </div>
     </Contenedor>
   );
